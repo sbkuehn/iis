f58e6751a37ca50e0a5c1ef03625c5f07df50fc4
sbkuehn <skuehn@10thmagnitude.com>
2017-03-16 21:50:17 +0000

    updated changes

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,9 +9,14 @@ powershell_script 'Install IIS' do
 end
 
 file 'c:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+  <h2>ipaddress: #{node['ipaddress']}</h2>
+  <h2>hostname: #{node['hostname']}</h2>
+  <h2>total memory: #{node['memory']['total']}</h2>
+  <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
 end
 
 service 'w3svc' do
   action [:enable, :start]
 end
+
