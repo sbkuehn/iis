1c9df124a73a326292b8ce6fb656636a0410a336
sbkuehn <skuehn@10thmagnitude.com>
2017-03-17 15:02:12 +0000

    Template changes.

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,12 +8,8 @@ powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
 end
 
-file 'c:\inetpub\wwwroot\Default.htm' do
-  content "<h1>Hello, world!</h1>
-  <h2>ipaddress: #{node['ipaddress']}</h2>
-  <h2>hostname: #{node['hostname']}</h2>
-  <h2>total memory: #{node['memory']['total']}</h2>
-  <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
+template 'c:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
